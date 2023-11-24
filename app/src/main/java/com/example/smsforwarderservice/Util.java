e7fe53709e4a88c3a8ddd6064bb422367c3ad415
aritra mukherjee <aritramukherjee@aritras-MacBook-Air.local>
2023-11-25 01:50:00 +0530

    Release 1.0.0 commit

--- app/src/main/java/com/example/smsforwarderservice/Util.java
+++ app/src/main/java/com/example/smsforwarderservice/Util.java
@@ -4,15 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
 public class Util {
+    private static final String TAG = "SMSForwarder";
     public static void processSMSReceivedIntent(Context context, Intent intent){
         if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
-            //if(intent.getAction().equals("Telephony.Sms.Intents.SMS_RECEIVED_ACTION")){
-            System.out.println("intent.hi()=>");
+            Log.d(TAG, "SMS/Intent received");
             Toast.makeText(context, "SMS Received", Toast.LENGTH_SHORT).show();
             Bundle bundle = intent.getExtras();
             SmsMessage[] msgs = null;
@@ -22,26 +21,27 @@ public class Util {
                     Object[] pdus = (Object[]) bundle.get("pdus");
                     msgs = new SmsMessage[pdus.length];
                     for(int i=0; i<pdus.length; i++){
-                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);//, "");
+                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
                         msg_from = msgs[i].getOriginatingAddress();
                         String msgBody = msgs[i].getMessageBody();
-                        Toast.makeText(context, "From : "+ msg_from + "Content : " + msgBody, Toast.LENGTH_SHORT).show();
-                        // System.out.println("intent.hi2()=>" + "From : "+ msg_from + "Content : " + msgBody);
+                        String result = "From : " + msg_from + "Content : " + msgBody;
+                        Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "SMS Content : " + result);
                     }
+                    Log.d(TAG, "All Messages are processed =>" + msgs != null ? "Y" : "N");
+                    sendToSalesforce(msgs);
+                    Log.d(TAG, "Send To Salesforce is finished");
                 }
                 catch(Exception e){
+                    Log.e(TAG, "Error occurred in processing SMS in receiver :" + e.getMessage());
                     Toast.makeText(context, "From : "+ e.getMessage() + "Content : " + e.getCause(), Toast.LENGTH_SHORT).show();
                     e.printStackTrace();
                 }
-                finally{
-                    saveRecords(msgs);
-                }
             }
         }
-
     }
 
-    private static void saveRecords(SmsMessage[] msgs) {
+    private static void sendToSalesforce(SmsMessage[] msgs) {
         if(msgs != null) {
             new SFUtil().execute(msgs);
         }
